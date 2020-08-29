e7b68ef21e81ee1457a119ff759e9bfadfcd0150
Chukwudumebi-Obi <joshman_234@hotmail.com>
2020-08-29 02:23:04 -0400

    merge changes?

--- benchfinder.py
+++ benchfinder.py
@@ -1,28 +1,69 @@
 from flask import Flask, redirect, url_for, render_template, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField
+import folium
+import collections
+import pandas
 from flask_wtf import FlaskForm
-
+#lol ok this is creative
+#this one is really more creative
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'whatthefrick'
+data=pandas.read_csv('Benches_Coordinates.csv')
+#okay
+LAT=list(data['Latitude'])
+LON=list(data['Longitude'])
+name=list(data['Name'])
+directions=list(data['Directions'])
+LIST_BENCHES=name
+
+start_coords = [42.4453,-76.482661]
+fg=folium.Map(location=start_coords, zoom_start=17)
+locationBench=[]
+popupBench=[]
+
+
+for i in range(13):
+
+    locationBench_temp = [LAT[i], LON[i]]
+    locationBench.append(locationBench_temp)
+    description = "<b>Name  : </b>"+name[i] + "<br><b>Directions: </b><a href="+directions[i]+">click here</a>"
+    popupBench.append(description)
+    folium.Marker(location=locationBench_temp, popup=description, icon=folium.features.CustomIcon('bench.jpg', icon_size=(50,50))).add_to(fg)
 
 
 class ReusableForm(FlaskForm):
-    username = StringField('Username')
+    username = StringField('Number of People Going')
     submit = SubmitField('Submit')
+    benches = SelectField(label='Benches', choices=[(bench, bench) for bench in LIST_BENCHES])
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/send", methods=['GET', 'POST'])
+@app.route('/')
+def index():
+    return fg._repr_html_()
+
+
+@app.route('/density')
+def density():
+    return render_template("density.html")
+
+@app.route('/cumap')
+def mapdisplay():
+    return fg._repr_html_()
+
+@app.route('/send', methods=['GET', 'POST'])
 def send():
-    # dropdown_list = ['The Slope Benches','Class of 1927 Bench']
     form= ReusableForm()
-    if form.validate_on_submit:
-        return render_template('persons.html',form=form)
-    return render_template("index.html",form=form)
+
+    if form.validate_on_submit():
+        
+        user = "Got it! {0} more hoomans are visiting {1}.".format(form.username.data, form.benches.data)
+        return render_template('submission.html', value=user)
+        #  return "<h2> Persons going: {0} {1}".format(form.username.data, form.benches.data)
+    return render_template("persons.html",form=form)
 
 @app.route("/testsignup")
 def signup():
